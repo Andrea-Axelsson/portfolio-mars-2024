a07b898e3032be8e46a3b1b636b8491507041fa3
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-03-02 18:09:07 +0100

    Updated code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,27 +4,66 @@ const Projects = () => {
         <h1 className="header header--light">Projects</h1>
         <section className="projects-container">
         
-        <section className="project">
+            <section className="project">
+                <h2 className="project__header">Creative Portfolio</h2>
+                <article className="project__image">
+                </article>
+                <section className="project__skill-tag-group">
+
+                            <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+
+                            <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+
+                            <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+                            <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+
+                            <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+                    </section>
+                <p className="body-text body-text--white">
+                Lorem ipsum dolor sit amet consectetur. Metus turpis pellentesque volutpat odio aliquam. Tortor ultrices facilisi ac in scelerisque. Tortor elit tincidunt lacus ultrices dictumst. Cursus odio euismod volutpat quam fusce nisi enim. Pretium nunc nullam nibh sit quisque 
+                </p>
+                <section className="project__buttons">
+                    <article className="button-tag">
+                            <article className="button-tag-info">
+                                <img className="button-tag-icon" src="/github.svg" alt="Github icon"/> 
+                                <p className="body-text body-text--white">Code</p>
+                            </article>
+                        </article>
+                        <article className="button-tag">
+                            <article className="button-tag-info">
+                                <img className="button-tag-icon" src="/link.png" alt="Link icon"/> 
+                                <p className="body-text body-text--white">Live</p>
+                            </article>
+                        </article>
+                </section>
+
+            </section>
+
+            <section className="project">
             <h2 className="project__header">Creative Portfolio</h2>
             <article className="project__image">
-                <section className="project__skill-tag-group">
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                </section>
             </article>
+            <section className="project__skill-tag-group">
+
+                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+
+                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+
+                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+
+                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
+
+                </section>
             <p className="body-text body-text--white">
             Lorem ipsum dolor sit amet consectetur. Metus turpis pellentesque volutpat odio aliquam. Tortor ultrices facilisi ac in scelerisque. Tortor elit tincidunt lacus ultrices dictumst. Cursus odio euismod volutpat quam fusce nisi enim. Pretium nunc nullam nibh sit quisque 
             </p>
@@ -44,46 +83,8 @@ const Projects = () => {
             </section>
 
         </section>
-        <section className="project">
-            <h2 className="project__header">Creative Portfolio</h2>
-            <article className="project__image">
-                <section className="project__skill-tag-group">
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                    <article className="project__skill-tag">
-                        <img className="project__skill-logo" src="/html.svg" alt="Tech logo"/> 
-                    </article>
-                </section>
-            </article>
-            <p className="body-text body-text--white">
-            Lorem ipsum dolor sit amet consectetur. Metus turpis pellentesque volutpat odio aliquam. Tortor ultrices facilisi ac in scelerisque. Tortor elit tincidunt lacus ultrices dictumst. Cursus odio euismod volutpat quam fusce nisi enim. Pretium nunc nullam nibh sit quisque 
-            </p>
-            <section className="project__buttons">
-                <article className="button-tag">
-                        <article className="button-tag-info">
-                            <img className="button-tag-icon" src="/github.svg" alt="Github icon"/> 
-                            <p className="body-text body-text--white">Code</p>
-                        </article>
-                    </article>
-                    <article className="button-tag">
-                        <article className="button-tag-info">
-                            <img className="button-tag-icon" src="/link.png" alt="Link icon"/> 
-                            <p className="body-text body-text--white">Live</p>
-                        </article>
-                    </article>
-            </section>
 
-        </section>
+
         </section>
         
     </section>
