0e932b41040eb299741eece07dabff6ef61cecec
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2024-03-12 20:11:13 +0100

    Updated code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,24 +1,13 @@
-
-// Imports the HorizontalScrollCarousel component, which is responsible for displaying the projects in a horizontally scrolling format.
-import HorizontalScrollCarousel from './HorizontalScrollCarousel'
+import { ProjectProps } from "./Interface"
 
 
-const Projects = () => {
+const Projects = (props:ProjectProps) => {
   return (
 
     <section id="projects" className="projects-background">
-    
-    <article className='projects__header-arrow'>
       <h1 className="header header--light">Projects</h1>
-      <div className='scroll-arrow'>
-        <p className='body-text body-text--white'>scroll</p>
-        <i className="fa-solid fa-arrow-right"></i>
-      </div>
-      
-    </article>
-    {/* Section container for the HorizontalScrollCarousel component */}
     <section className="projects-container">
-        <HorizontalScrollCarousel/> {/* Renders the horizontal scroll carousel with project details */}
+        {props.portfolioProject}
     </section>
     </section>
   )
