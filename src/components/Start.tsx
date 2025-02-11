0872573047739c2a90f255190e1bc57d6ec88656
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2025-02-11 19:19:48 +0100

    Update Start.tsx

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,15 +1,15 @@
-
-
 const Start = () => {
-    return (
-
+  return (
     <section className="start">
-        <img src="/andrea.jpg" alt="A picture of Andrea" className="start__image"/>
-             <h2 className="start__header-1">Hi, I am</h2>
-            <h1 className="header start__header-2" >Andrea Axelsson</h1>
-            <h2 className="start__sub-header">Front end developer</h2>
-    </section> 
-    )
-}
-export default Start
-
+      <img
+        src="/andrea.jpg"
+        alt="A picture of Andrea"
+        className="start__image"
+      />
+      <h2 className="start__header-1">Hi, I am</h2>
+      <h1 className="header start__header-2">Andrea Axelsson</h1>
+      <h2 className="start__sub-header">Frontend developer</h2>
+    </section>
+  );
+};
+export default Start;
